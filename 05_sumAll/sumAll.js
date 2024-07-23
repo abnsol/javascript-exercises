b5a4775bf57cd9ab295dd2ffecb162433df3cd99
abnsol <abenisolo51196@gmail.com>
2024-07-24 00:09:44 +0300

    solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(numOne,numTwo) {
+    if (numOne < 0 || numTwo < 0){
+        return "ERROR";
+    }else if(typeof(numOne) !== 'number' || typeof(numTwo) != 'number'){
+        return "ERROR";
+    }else if(Math.floor(numOne) != numOne || Math.floor(numTwo) != numTwo){
+        return "ERROR";
+    }else{
+        const n = (Math.max(numTwo,numOne) + 1) - Math.min(numOne,numTwo);
+        return n * ((numTwo + numOne)/2);
+    }
 };
 
 // Do not edit below this line
