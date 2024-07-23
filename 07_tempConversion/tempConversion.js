466af09e456fd72692ac56390d41e05d7514d3c1
abnsol <abenisolo51196@gmail.com>
2024-07-24 00:37:15 +0300

    solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(((5/9)*(fahrenheit - 32)) * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  return Math.round(((9/5)*(celcius) + 32) * 10)/10;
 };
 
 // Do not edit below this line
